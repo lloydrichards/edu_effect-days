e8baccc0985866011c95fb716e7a3221163dd40e
lloydrichards <lloyd.d.richards@gmail.com>
2025-03-19 07:49:21 +0100

    build: :tada: initialize project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Console, Effect, pipe } from "effect";
+
+const program = pipe(
+  Effect.succeed(42),
+  Effect.tap(() => Console.info("Hello via Effect!"))
+);
+
+const run = Effect.runSync(program);
